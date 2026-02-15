ee7732ce0a2ef4259056a3f844ef495a3fd3c062
ezrachai <juanzhe2@gmail.com>
2026-02-15 17:19:26 +0800

    fix: correct import path casing for LinkDevice component in LinkDevicePage

--- app/link-device/page.tsx
+++ app/link-device/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import LinkDevice from "../../components/LinkDevice";
+import LinkDevice from "@/components/Linkdevice";
 import { Loader } from "lucide-react";
 import { Suspense } from "react";
 
